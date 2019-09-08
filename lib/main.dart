b50327e1531341a8fd3dbab1734dfb85dd6cf552
Ndjock Junior <ndjockjunior@gmail.com>
2019-09-08 16:35:33 +0100

    good

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'My Flutter CV',
         home: Scaffold(
             appBar: AppBar(title: Text("My Flutter CV",textAlign: TextAlign.center)),
             body: Container(
